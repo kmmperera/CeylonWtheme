94006e68b0b9f06a92bb5ffb127f72dd7e08a652
mahesh perera <maheshmadusankap@gmail.com>
2024-06-17 20:51:13 +0200

    paymentuploadfx22222

--- page-uploadimages.php
+++ page-uploadimages.php
@@ -6,13 +6,34 @@
 
     <div class="mainbody h-padding">
 
-
+            <div class="payslip-page-title">
+                Upload your Pay Slip
+            </div>
        
             <form id="featured_upload" method="post" action="#" enctype="multipart/form-data">
-            
-                <input type="text" name="post_id" id="post_id" value="" /> 
-                <input type="file" name="my_image_upload" id="my_image_upload"  multiple="false" />
-            
+
+                    <table class="input-table">
+                            <tr>
+                                <td> <label for="post_id">User Code:</label></td>
+                                <td> <input type="number" name="post_id" id="post_id" value="" placeholder="Enter your code" /></td>
+
+                            </tr>
+                            <tr>
+                                <td><label for="my_image_upload">Pay Slip:</label></td>
+                                <td>  <input type="file" name="my_image_upload" id="my_image_upload"  multiple="false" /> </td>
+                            
+                            </tr>
+
+                    </table>
+                <!-- <div class="input-container">
+                    <label for="post_id">User Code:</label>
+                    <input type="text" name="post_id" id="post_id" value="" /> <br>
+                </div>
+                
+                <div class="input-container">
+                    <label for="my_image_upload">Pay Slip:</label>
+                    <input type="file" name="my_image_upload" id="my_image_upload"  multiple="false" /> <br>
+                </div> -->
 
                 <?php wp_nonce_field( 'my_image_upload', 'my_image_upload_nonce' ); ?>
 
